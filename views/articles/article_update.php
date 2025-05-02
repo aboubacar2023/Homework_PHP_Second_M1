f3c813f03a9c71cb65ca7954a05a04ef14aa01ed
Aboubacar BANE <aboubacarbane@gmail.com>
2025-05-02 08:32:46 +0000

    mise à jour sur les vues

--- views/articles/article_update.php
+++ views/articles/article_update.php
@@ -19,7 +19,7 @@ $query_2 = $db->prepare($sqlQueryCat);
 $query_2->execute();
 $categories = $query_2->fetchAll();
 ?>
-<div class="content-area overflow-y-auto p-6">
+<div class="content-area overflow-y-auto p-6 m-6">
     <div class="mb-6 w-full">
         <a href="article_index.php"><button type="submit"
                 class="py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-800 hover:bg-blue-900">
@@ -53,9 +53,9 @@ $categories = $query_2->fetchAll();
                     </select>
                 </div>
             </div>
-            <div>
+            <div class="flex justify-center text-center">
                 <button type="submit"
-                    class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-800 hover:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                    class="w-60 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-800 hover:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                     Modifier
                 </button>
             </div>
